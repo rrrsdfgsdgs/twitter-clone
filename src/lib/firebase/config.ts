405ad7a8714b2bd9d5b49270bfdd23ca4bc09375
rrrsdfgsdgs <itslucidpp@gmail.com>
2025-06-28 17:04:30 -0400

    Update config.ts

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,11 +1,11 @@
 const config = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+  apiKey: process.env.AIzaSyD_hz1uDLweZyF2vdH5_XIjH5AE3MheO80,
+  authDomain: process.env.hhhmhvn.firebaseapp.com,
+  projectId: process.env.hhhmhvn,
+  storageBucket: process.env.hhhmhvn.firebasestorage.app,
+  messagingSenderId: process.env.699200122926,
+  appId: process.env.1:699200122926:web:920be98a6cd547999f6fbf,
+  measurementId: process.env.G-XBWRH9KKG7
 } as const;
 
 type Config = typeof config;
