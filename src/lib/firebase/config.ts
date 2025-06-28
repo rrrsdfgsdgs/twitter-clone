a0f71449ee98940d538ba255b82262334c63b926
rrrsdfgsdgs <itslucidpp@gmail.com>
2025-06-28 17:52:32 -0400

    Update config.ts

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,18 +1,9 @@
-const config = {
-  apiKey: process.env.AIzaSyD_hz1uDLweZyF2vdH5_XIjH5AE3MheO80,
-  authDomain: process.env.hhhmhvn.firebaseapp.com,
-  projectId: process.env.hhhmhvn,
-  storageBucket: process.env.hhhmhvn.firebasestorage.app,
-  messagingSenderId: process.env.699200122926,
-  appId: process.env.1:699200122926:web:920be98a6cd547999f6fbf,
-  measurementId: process.env.G-XBWRH9KKG7
-} as const;
-
-type Config = typeof config;
-
-export function getFirebaseConfig(): Config {
-  if (Object.values(config).some((value) => !value))
-    throw new Error('Firebase config is not set or incomplete');
-
-  return config;
+export function getFirebaseConfig() {
+  return {
+    apiKey: process.env.AIzsaSyD_hz1uDLweZyF2vdH5_XIjH5AE3Mh || "",
+    authDomain: process.env.hhhmhvn_firebaseapp_com || "",
+    projectId: process.env.hhhmhvn || "",
+    storageBucket: process.env.hhhmhvn_firebasestorage_app || "",
+    messagingSenderId: process.env["699200122926"] || "",
+  };
 }
